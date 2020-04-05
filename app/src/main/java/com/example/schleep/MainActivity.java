a2882516af81280a6b09553fbdf10860fc8b95f7
vsysms <viviansys@outlook.com>
2020-04-05 03:25:55 -0400

    Created new toolbar w/ chewy font
    Added fab to main activity

--- app/src/main/java/com/example/schleep/MainActivity.java
+++ app/src/main/java/com/example/schleep/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.schleep;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar schleepToolbar;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        schleepToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(schleepToolbar);
+        getSupportActionBar().setTitle(null);
     }
 }

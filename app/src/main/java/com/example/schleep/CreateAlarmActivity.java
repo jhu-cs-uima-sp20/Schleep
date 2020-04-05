d1f0c614d8efd4ea7ee3f4d42abe60fe8deeb082
Vivian <50971283+vsysms@users.noreply.github.com>
2020-04-05 03:57:14 -0400

    added cute sheep svg (we can change it)

---
+++ app/src/main/java/com/example/schleep/CreateAlarmActivity.java
@@ -0,0 +1,22 @@
+package com.example.schleep;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class CreateAlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.createalarm_activity);
+
+    }
+
+}
--- app/src/main/java/com/example/schleep/MainActivity.java
+++ app/src/main/java/com/example/schleep/MainActivity.java
@@ -3,13 +3,19 @@ package com.example.schleep;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar schleepToolbar;
+    private FloatingActionButton createAlarmButton;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -17,8 +23,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /* This code sets up the schleep Toolbar with the chewy font */
         schleepToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(schleepToolbar);
         getSupportActionBar().setTitle(null);
+
+        /* When the floating action button is pressed, we will go to the Create
+        Alarm activity.
+         */
+        createAlarmButton = (FloatingActionButton) findViewById(R.id.createAlarmButton);
+        createAlarmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //here
+                startActivity(new Intent(MainActivity.this, CreateAlarmActivity.class));
+            }
+        });
     }
 }

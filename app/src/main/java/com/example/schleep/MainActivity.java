0f57aaa8a4da8960b580f6340dc4b68ee0357f33
Jessica  Nguyen/ <jnguye45@jhu.edu>
2020-04-06 20:44:12 -0500

    broken recyler view

--- app/src/main/java/com/example/schleep/MainActivity.java
+++ app/src/main/java/com/example/schleep/MainActivity.java
@@ -1,43 +1,90 @@
 package com.example.schleep;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+}
+
+
+/*
+public class MainActivity extends AppCompatActivity implements LoadAlarmsReceiver.OnAlarmsLoadedListener {
+
     private Toolbar schleepToolbar;
     private FloatingActionButton createAlarmButton;
+    private AlarmsAdapter mAdapter;
+    public static final int EDIT_ALARM = 1;
+    public static final int ADD_ALARM = 2;
+   private LoadAlarmsReceiver mReceiver;
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+*/
         /* This code sets up the schleep Toolbar with the chewy font */
-        schleepToolbar = (Toolbar) findViewById(R.id.toolbar);
+     /*   schleepToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(schleepToolbar);
         getSupportActionBar().setTitle(null);
+        mReceiver = new LoadAlarmsReceiver(this);
 
-        /* When the floating action button is pressed, we will go to the Create
-        Alarm activity.
-         */
         createAlarmButton = (FloatingActionButton) findViewById(R.id.createAlarmButton);
         createAlarmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //here
+                //final Intent i = buildAddEditAlarmActivityIntent(MainActivity.class, ADD_ALARM);
+                //startActivity(i);
                 startActivity(new Intent(MainActivity.this, CreateAlarmActivity.class));
             }
-        });
+        });*/
+        /* When the floating action button is pressed, we will go to the Create
+        Alarm activity.
+         */
+       /* EmptyRecyclerView rv = findViewById(R.id.recycler);
+        mAdapter = new AlarmsAdapter();
+        rv.setEmptyView(findViewById(R.id.empty_view));
+        rv.setAdapter(mAdapter);
+        LinearLayoutManager lm = new LinearLayoutManager(this);
+        rv.addItemDecoration(new DividerItemDecoration(rv.getContext(), lm.getOrientation()));
+        rv.setLayoutManager(lm);
+        rv.setItemAnimator(new DefaultItemAnimator());
+        Toast.makeText(getBaseContext(), mAdapter.getItemCount() + " items", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        final IntentFilter filter = new IntentFilter(LoadAlarmsService.ACTION_COMPLETE);
+        LocalBroadcastManager.getInstance(getBaseContext()).registerReceiver(mReceiver, filter);
+        LoadAlarmsService.launchLoadAlarmsService(this);
+        Toast.makeText(this, mAdapter.getItemCount() + " items", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onAlarmsLoaded(ArrayList<Alarm> alarms) {
+        mAdapter.setAlarms(alarms);
     }
 }
+*/
---
+++ app/src/main/java/com/example/schleep/MainFragment.java
@@ -0,0 +1,81 @@
+package com.example.schleep;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+import static com.example.schleep.AddEditAlarmActivity.ADD_ALARM;
+import static  com.example.schleep.AddEditAlarmActivity.buildAddEditAlarmActivityIntent;
+
+public final class MainFragment extends Fragment
+        implements LoadAlarmsReceiver.OnAlarmsLoadedListener {
+
+    private LoadAlarmsReceiver mReceiver;
+    private AlarmsAdapter mAdapter;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mReceiver = new LoadAlarmsReceiver(this);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        final View v = inflater.inflate(R.layout.activity_main, container, false);
+
+        final EmptyRecyclerView rv = v.findViewById(R.id.recycler);
+        mAdapter = new AlarmsAdapter();
+        rv.setEmptyView(v.findViewById(R.id.empty_view));
+        rv.setAdapter(mAdapter);
+        rv.addItemDecoration(new DividerItemDecoration(getContext()));
+        rv.setLayoutManager(new LinearLayoutManager(getContext()));
+        rv.setItemAnimator(new DefaultItemAnimator());
+
+        final FloatingActionButton fab = v.findViewById(R.id.createAlarmButton);
+        fab.setOnClickListener(view -> {
+            //AlarmUtils.checkAlarmPermissions(getActivity());
+            final Intent i = buildAddEditAlarmActivityIntent(getContext(), ADD_ALARM);
+            startActivity(i);
+        });
+
+        return v;
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        final IntentFilter filter = new IntentFilter(LoadAlarmsService.ACTION_COMPLETE);
+        LocalBroadcastManager.getInstance(getContext()).registerReceiver(mReceiver, filter);
+        LoadAlarmsService.launchLoadAlarmsService(getContext());
+        Toast.makeText(getContext(), mAdapter.getItemCount() + " items", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        LocalBroadcastManager.getInstance(getContext()).unregisterReceiver(mReceiver);
+    }
+
+    @Override
+    public void onAlarmsLoaded(ArrayList<Alarm> alarms) {
+        mAdapter.setAlarms(alarms);
+    }
+
+}

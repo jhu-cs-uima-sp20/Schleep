9f13211ea0c9cc369f15747f10b3c13c48ea1a86
Jessica  Nguyen/ <jnguye45@jhu.edu>
2020-04-27 18:59:58 -0500

    fragment alarm pages change dynamically

--- app/src/main/java/com/example/schleep/AlarmLandingPageActivity.java
+++ app/src/main/java/com/example/schleep/AlarmLandingPageActivity.java
@@ -6,12 +6,15 @@ import android.content.pm.ActivityInfo;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public final class AlarmLandingPageActivity extends AppCompatActivity {
-
+    private static final int CONTENT_VIEW_ID = 10101010;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
@@ -25,7 +28,18 @@ public final class AlarmLandingPageActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_landing_page);
-
+/*
+        Fragment newFragment = new AlarmLandingPageFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(CONTENT_VIEW_ID, newFragment).commit();
+        */
+//landing_page_fragment_container
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+// Replace the contents of the container with the new fragment
+        ft.replace(R.id.landing_page_fragment_container, new AlarmLandingPageFragmentMath());
+// or ft.add(R.id.your_placeholder, new FooFragment());
+// Complete the changes added above
+        ft.commit();
     }
 
     public static Intent launchIntent(Context context) {
